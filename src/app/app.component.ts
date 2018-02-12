4205c996d2b394d7a8389a435f282fabdcd8e94d
kidman90 <kacprzak90@gmail.com>
2018-02-12 23:41:51 +0100

    07 Loop Through Angular 2 Components with ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,21 +4,19 @@ import { Component, Inject } from '@angular/core';
   selector: 'app-root',
   template: `<div>
 
-  <app-simple-form></app-simple-form>
-
-  {{mail.message}}
-  <hr>
-  {{api}}
+<ul>
+  <li *ngFor="let message of mail.messages">
+    {{message}}
+  </li>
+</ul>
   
   </div>`
   // templateUrl: './app.component.html',
   // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = `Let's get started!`;
 
   constructor(
-    @Inject('mail') private mail,
-    @Inject('api') private api
+    @Inject('mail') private mail
   ) { }
 }
