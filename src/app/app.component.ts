e699180aa7491a212d345feff3e13995c084ebac
kidman90 <kacprzak90@gmail.com>
2018-02-21 22:30:24 +0100

    11 Write CSS Inside of Angular 2 Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,12 @@ import { Component, Inject } from '@angular/core';
 </app-simple-form>
 
   
-  </div>`
+  </div>`,
+  styles: [`
+    app-simple-form {
+      margin-bottom: 10px;
+    }
+  `]
   // templateUrl: './app.component.html',
   // styleUrls: ['./app.component.css']
 })
--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -3,13 +3,29 @@ import { EventEmitter } from '@angular/core/src/facade/async';
 
 @Component({
   selector: 'app-simple-form',
-  template: `<div>
-    
+  template: `
     <input #myInput type="text" [(ngModel)]="message">
     <button (click)="update.emit({ text: message })">Click me!</button>
+  `,
+  styles: [`
+    :host {
+      display: flex;
+      flex-direction: column;
+    }
 
-  </div>`,
-  styles: []
+    * {
+      font-family: monospace;
+    }
+
+    input:focus {
+      font-weight: bold;
+      outline: none;
+    }
+
+    button {
+      border: none;
+    }
+  `]
 })
 export class SimpleFormComponent implements OnInit {
 
